a5a4a358095999521157f53cd3b5158a42c42d18
Ananyasbabu <ananyasbabu2005@gmail.com>
2025-02-11 12:57:27 +0530

    second day react

---
+++ 11/02/2025/examples/src/App.jsx
@@ -0,0 +1,12 @@
+import Greetings from "./greetings";
+
+export default function App(){
+  return(
+    <>
+    <h1><marquee behavior="scroll" direction="center" scrollamount="50"> Welcome to React Ananya</marquee></h1>
+    <Greetings/>
+
+    </>
+  );
+}
+// export default App;
---
+++ 11/02/2025/examples/src/Greetings.jsx
@@ -0,0 +1,9 @@
+function Greetings(){
+    return(
+        <>
+            <h3>Hi i am greeting child component</h3>
+        </>
+    );
+
+}
+export default Greetings;
